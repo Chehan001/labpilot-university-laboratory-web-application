167fd45adc755d101311c9dd4e3595d6b9b16472
Chehan Lasindu <clmallikage001@gmail.com>
2025-12-12 07:20:25 +0530

    fixed-Errors

--- frontend/src/firebase/config.js
+++ frontend/src/firebase/config.js
@@ -1,20 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getStorage } from "firebase/storage";  
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAZbTlAGaA4B3LJzAUTJOLZpfa-hLnZ0o4",
+  authDomain: "labpilot-1c4e9.firebaseapp.com",
+  projectId: "labpilot-1c4e9",
+  storageBucket: "labpilot-1c4e9.appspot.com", 
+  messagingSenderId: "747117539626",
+  appId: "1:747117539626:web:27dee5ded89d0284cb2e53",
+  measurementId: "G-1KCDQL7ZR4",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const storage = getStorage(app); 
